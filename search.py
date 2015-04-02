9f345837443e9fe5348982c7812d4d31e1a61128
zpf <zpf@highwe.com>
2015-04-02 14:15:25 +0800

    添加README等等

--- search.py
+++ search.py
@@ -12,21 +12,23 @@ class SearchHandler(tornado.web.RequestHandler):
     def get(self):
         if not self.get_secure_cookie("user_id"):
             self.set_secure_cookie("user_id", str(random.randint(100, 10000)))
-            self.write("API Searching!")
+            self.write("Cookie is already seted, please frash to conitue !")
         else:
-            self.write("Come on Template!")
+            self.render("index.html")
 
     def post(self):
         pass
 
 
-settings={
+settings = {
     "static_path": os.path.join(os.path.dirname(__file__), "static"),
-    "cookie_secret": "rui_zhao_together"
+    "cookie_secret": "rui_zhao_together",
+    "template_path": "./template",
+    "debug": True
 }
 
 application = tornado.web.Application([(r"/", SearchHandler),
-                                     (r"/static/(.*)", tornado.web.StaticFileHandler, dict(path = settings["static_path"]))], **settings)
+    (r"/static/(.*)", tornado.web.StaticFileHandler, dict(path = settings["static_path"]))], **settings)
 
 if __name__ == "__main__":
     if len(sys.argv) == 2:
